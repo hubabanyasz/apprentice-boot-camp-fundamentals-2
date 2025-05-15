86501d5b8caf9fd15c592c82aa1c90a4c4c5a762
Huba Banyasz <huba.banyasz@autotrader.co.uk>
2025-05-15 14:54:24 +0100

    More refactor

--- exercises/java/src/main/java/refactoring/FizzBuzz.java
+++ exercises/java/src/main/java/refactoring/FizzBuzz.java
@@ -10,37 +10,42 @@ class FizzBuzz {
     public static final int ONE_HUNDRED = Byte.MAX_VALUE - 27;
     public static final int THREE_IN_BINARY = 0b11;
     public static final int FIVE = new int[]{0, 0, 0, 0, 0}.length;
+    public static final String FIZZ_HEX = "46697a7a";
+    public static final String BUZZ_HEX = "42757a7a";
     private int countsUpToOneHundred;
     private int countsUpToThree;
     private int countsDownFromFive = FIVE;
 
     String doFizzBuzzUpTo100() {
-        String resultString = "";
+        StringBuilder resultString = new StringBuilder();
         for (; countsUpToOneHundred < ONE_HUNDRED; countsUpToOneHundred++) {
             String fizzOrBuzzPlusASpace = addFizzOrBuzz(countsUpToOneHundred) + " ";
-            resultString += fizzOrBuzzPlusASpace;
+            resultString.append(fizzOrBuzzPlusASpace);
         }
-        String removeLastCharacter = resultString.substring(0, resultString.length() - 1);
-        return removeLastCharacter;
+        return resultString.substring(0, resultString.length() - 1);
     }
 
     private String addFizzOrBuzz(int number) {
         countsUpToThree++;
         countsDownFromFive--;
-        boolean isCountsDownFromFiveDivisibleByFive = countsDownFromFive == 0;
-        boolean isCountsDownFromThreeDivisibleByThree = countsUpToThree == THREE_IN_BINARY;
+
+        boolean isDivisibleByFive = countsDownFromFive == 0;
+        boolean isDivisibleByThree = countsUpToThree == THREE_IN_BINARY;
+
         String currentNumberInTheLoop = String.valueOf(number + 1);
-        String fizzOrBuzz = isCountsDownFromThreeDivisibleByThree || isCountsDownFromFiveDivisibleByFive ? "" : currentNumberInTheLoop;
-        if (isCountsDownFromThreeDivisibleByThree) fizzOrBuzz += fizz();
-        if (isCountsDownFromFiveDivisibleByFive) fizzOrBuzz += buzz();
+        boolean isDivisibleByThreeOrFive = isDivisibleByThree || isDivisibleByFive;
+        String fizzOrBuzz = isDivisibleByThreeOrFive ? "" : currentNumberInTheLoop;
+
+        if (isDivisibleByThree) fizzOrBuzz += fizz();
+        if (isDivisibleByFive) fizzOrBuzz += buzz();
+
         return fizzOrBuzz;
     }
 
     private String buzz() {
         countsDownFromFive = FIVE;
         try {
-            byte[] buzzInHexcode = Hex.decodeHex("42757a7a");
-            return new String(buzzInHexcode, StandardCharsets.UTF_8);
+            return new String(Hex.decodeHex(BUZZ_HEX), StandardCharsets.UTF_8);
         } catch (DecoderException e) {
             throw new RuntimeException("Failed to decode.", e);
         }
@@ -49,8 +54,7 @@ class FizzBuzz {
     private String fizz() {
         countsUpToThree = 0;
         try {
-            byte[] fizzInHexcode = Hex.decodeHex("46697a7a");
-            return new String(fizzInHexcode, StandardCharsets.UTF_8);
+            return new String(Hex.decodeHex(FIZZ_HEX), StandardCharsets.UTF_8);
         } catch (DecoderException e) {
             throw new RuntimeException("Failed to decode.", e);
         }
